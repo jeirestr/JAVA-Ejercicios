75f213d9307b83f0e61c950c71eae5ec924a2421
jeirestr <jeirestr@bancolombia.com.co>
2026-06-24 08:44:02 -0500

    Agregar ejercicio 5 completo

--- ejercicio3-4/java-bases/applications/app-service/src/main/java/co/com/bancolombia/ejercicio5_microservicios_demo/cotizador_service/CotizadorServiceApplication.java
+++ ejercicio3-4/java-bases/applications/app-service/src/main/java/co/com/bancolombia/ejercicio5_microservicios_demo/cotizador_service/CotizadorServiceApplication.java
@@ -1,4 +1,13 @@
 package co.com.bancolombia.ejercicio5_microservicios_demo.cotizador_service;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class CotizadorServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(CotizadorServiceApplication.class);
+        app.setAdditionalProfiles("cotizador");
+        app.run(args);
+    }
 }
--- ejercicio3-4/java-bases/applications/app-service/src/main/java/co/com/bancolombia/ejercicio5_microservicios_demo/vehiculo_service/VehiculoServiceApplication.java
+++ ejercicio3-4/java-bases/applications/app-service/src/main/java/co/com/bancolombia/ejercicio5_microservicios_demo/vehiculo_service/VehiculoServiceApplication.java
@@ -1,4 +1,13 @@
 package co.com.bancolombia.ejercicio5_microservicios_demo.vehiculo_service;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class VehiculoServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(VehiculoServiceApplication.class);
+        app.setAdditionalProfiles("vehiculo");
+        app.run(args);
+    }
 }
